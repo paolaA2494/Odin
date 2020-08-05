9beded033273094944fd13003c226bc8f5bb31c8
Casta <palolahrn0104@gmail.com>
2020-08-05 09:12:24 -0500

    cree un proyecto recat, elimine archivos no necesarios y en app.js coloque mi tabla

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tabla de integrantes</h1>
+      <div class="table-responsive">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Apellidos</th>
+              <th>Identificación</th>
+              <th>Correo</th>
+              <th>Telefono</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Paola Andrea</td>
+              <td>Hernández Velásquez</td>
+              <td>222382893923</td>
+              <td>cmtecnica09@elpoli.edu.co</td>
+              <td>88912310391</td>
+            </tr>
+            <tr>
+              <td>mariano N</td>
+              <td>amet</td>
+              <td>consectetur</td>
+              <td>adipiscing</td>
+              <td>elit</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
